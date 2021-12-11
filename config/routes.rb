5c8013429b6da90d69b5a10204f3d57281ea8913
Shuheng Luo <robinlsh0809@gmail.com>
2021-12-11 12:44:23 +0800

    user controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :services
+      resources :services do
+        resources :bookings, only: [ :show, :create, :destroy ]
+      end
       devise_scope :user do
         post 'login', to: 'user_sessions#create', as: 'login'
       end
