f0a9280c62a0d9d46a97fb1a5311940cc98be9a9
Roxanne Zhao <roxannezhao199@gmail.com>
2021-12-11 12:07:51 +0800

    done bookings controller

---
+++ app/controllers/api/v1/bookings_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::BookingsController < Api::V1::BaseController
+  before_action :find_service
+  def show
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.service = @service
+    @booking.save
+  end
+
+  def destroy
+    @booking.destroy
+  end
+
+  private
+
+  def find_service
+    @service = Service.find(params[:service_id])
+  end
+
+  def booking_params
+    params.require(:user_id).permit(:service_id)
+  end
+
+end
