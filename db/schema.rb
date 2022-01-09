e09510a56af582e7757da36ae41744a0221401ed
Shuheng Luo <robinlsh0809@gmail.com>
2022-01-09 12:05:19 +0800

    change something in seed

--- db/schema.rb
+++ db/schema.rb
@@ -74,7 +74,6 @@ ActiveRecord::Schema.define(version: 2022_01_08_141211) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "mp_openid"
-    t.string "avatar"
     t.string "gender"
   end
 
