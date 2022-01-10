e5fb8219bc1fc4e7e652fd4931512acdbf28bb59
Shuheng Luo <robinlsh0809@gmail.com>
2022-01-10 23:08:52 +0800

    redirect all the pages to the right path

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [ :index, :show, :update]
+      resources :users, only: [ :index, :show, :update, :create]
       post '/login', to: 'users#login'
       resources :services
       resources :pets do
